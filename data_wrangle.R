0179b95ce08c0850f19eac32ec55e39fee4e607f
Valantra Home <valantrahome@Valantras-Mac-mini.local>
2016-10-11 19:39:27 -0400

    updated

--- data_wrangle.R
+++ data_wrangle.R
@@ -1,11 +1,10 @@
 library("dplyr")
 library("tidyr")
 df <-
-  read.csv(
-    "/Users/valantrahome/projects/data_science-r/refine.csv",
-    header = TRUE,
-    stringsAsFactors = FALSE
-  )
+  read.csv("./refine.csv",
+           header = TRUE,
+           stringsAsFactors = FALSE)
+
 
 refined_data = df %>%
   mutate(company = if_else(grepl('^[vV]', company), 'van houten', company)) %>%
@@ -20,6 +19,6 @@ refined_data = df %>%
   mutate(code = if_else(code == 'v', 'TV', code)) %>%
   mutate(code = if_else(code == 'x', 'Laptop', code)) %>%
   mutate(code = if_else(code == 'q', 'Tablet', code)) %>%
-  mutate(full_address = paste(address,",",city,",",country))
+  mutate(full_address = paste(address, ",", city, ",", country))
 
 refined_data %>% select(company, name, code, number, address, city, country, full_address) %>% arrange(company) %>% View()
