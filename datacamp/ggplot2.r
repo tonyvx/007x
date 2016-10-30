74532fc2eae59d3bb6f5ca0110d34a2a36dc8bc4
tony <anthony.valantra@gmail.com>
2016-10-30 15:00:33 -0400

    ggplot2 stats

--- datacamp/ggplot2.r
+++ datacamp/ggplot2.r
@@ -471,10 +471,78 @@ ggplot(titanic, aes(x=factor(Pclass),y=Age, col=factor(Sex))) +
   geom_jitter(position=posn.j, size=3,alpha=0.5) +
   facet_grid(".~Survived")
 
+# ggplot2 is already loaded
 
+# Explore the mtcars data frame with str()
+str(mtcars)
 
+# A scatter plot with LOESS smooth:
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() + geom_smooth()
 
+# A scatter plot with an ordinary Least Squares linear model:
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() + 
+  geom_smooth(method="lm")
 
+# The previous plot, without CI ribbon:
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() + 
+  geom_smooth(method="lm", se=FALSE)
+
+# The previous plot, without points:
+ggplot(mtcars, aes(x = wt, y = mpg)) + 
+  geom_smooth(method="lm", se=FALSE)
+
+  # ggplot2 is already loaded
+
+# Define cyl as a factor variable
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = F)
+
+# Complete the following ggplot command as instructed
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = F)+
+  stat_smooth(method = "lm", se = F,group=1)
+
+# Plot 1: change the LOESS span
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  # Add span below 
+  geom_smooth(se = F,span=0.7)
+
+# Plot 2: Set the overall model to LOESS and use a span of 0.7
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = F) +
+  # Change method and add span below
+  stat_smooth(method = "loess", aes(group = 1), 
+              se = F, col = "black", span=0.7)
+
+# Plot 3: Set col to "All", inside the aes layer of stat_smooth()
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = F) +
+  stat_smooth(method = "loess",
+              # Add col inside aes()
+              aes(group = 1,col="All"), 
+              # Remove the col argument below
+              se = F, span = 0.7)
+
+# Plot 4: Add scale_color_manual to change the colors
+myColors <- c(brewer.pal(3, "Dark2"), "black")
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = F, span = 0.75) +
+  stat_smooth(method = "loess", 
+              aes(group = 1, col="All"), 
+              se = F, span = 0.7) + 
+# Add correct arguments to scale_color_manual
+  scale_color_manual("Cylinders", values=myColors)
+  
+  
 
 
 
